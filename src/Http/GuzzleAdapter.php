dacf090ab7a295aee4bc57f9dd3ed7800ff12489
Ibrahima SOW <ibrahima.sow@prestashop.com>
2021-07-22 13:24:29 +0000

    Update license headers and install extension http

--- src/Http/GuzzleAdapter.php
+++ src/Http/GuzzleAdapter.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is authored by PrestaShop SA and Contributors <contact@prestashop.com>
+ *
+ * It is distributed under MIT license, since 2021.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
 
 namespace Help\PrestaShop\Http;
 
